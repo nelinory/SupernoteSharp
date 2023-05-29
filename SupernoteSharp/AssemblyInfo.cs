b7bd966e3abb8c1f2f9f52f5dae9d0e8842b985c
nelinory <15143882+nelinory@users.noreply.github.com>
2023-05-28 19:20:50 -0500

    Added assembly version

---
+++ SupernoteSharp/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyTitle("SupernoteSharp")]
+[assembly: AssemblyProduct("SupernoteSharp")]
+[assembly: AssemblyCopyright("Copyright © nelinory 2023")]
